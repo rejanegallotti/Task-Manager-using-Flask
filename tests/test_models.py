7b440d07d8f31b48b9bf3d7db1fd8f6a80e27b59
rejane <rejanemreis@hotmail.com>
2024-09-21 14:21:37 -0300

    so testando

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,5 +1,5 @@
 import unittest
-from models import User, Task
+from models import User
 
 class TestUserModelCase(unittest.TestCase):
 
@@ -12,20 +12,6 @@ class TestUserModelCase(unittest.TestCase):
     def test_user_repr(self):
         user = User(username="test_user")
         self.assertEqual(str(user), "User('test_user')")
-
-class TestTaskModelCase(unittest.TestCase):
-
-    def test_task_creation(self):
-        user = User(username="test_user", password="test_password")
-        task = Task(content="This is a test task", user_id=user.id)
-        self.assertEqual(task.content, "This is a test task")
-        self.assertIsNone(task.id)  # ID should be assigned after saving
-
-    def test_task_repr(self):
-        user = User(username="test_user")
-        task = Task(content="Test task", date_posted="2024-09-20 23:27:00", user_id=user.id)
-        self.assertEqual(str(task), "Task('Test task', '2024-09-20 23:27:00', 1)")  # Assuming user.id is 1
-
 
 if __name__ == '__main__':
     unittest.main()
